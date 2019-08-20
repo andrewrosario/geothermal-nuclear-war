2294c19fb13e03073ec66989272699d805174cbf
andrewrosario <rosario.andrew@gmail.com>
2019-08-19 19:20:03 -0500

    Clean up file structure, Populate cities table

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,8 +2,8 @@ class City < ActiveRecord::Base
     has_many :missiles
     has_many :games, through: :missiles
 
-    # def self.new_city(name, population, player)
-    #     new_city = City.new(name: name, population: population, player: player )
-    #     new_city.save
-    # end
+    def self.new_city(name, population, player)
+        new_city = City.new(name: name, population: population, player: player )
+        new_city.save
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,25 +3,9 @@ require 'pry'
 require 'yaml'
 require 'sinatra/activerecord'
 
-
 Bundler.require
-#require_all 'app'
-
-# ActiveRecord::Base.establish_connection({adapter: 'sqlite3', database: 'db/war_games.db'})
-
-#require_all 'lib'
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-# Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
-
-# DB = ActiveRecord::Base.establish_connection({
-#   adapter: 'sqlite3',
-#   database: 'db/tvshows.db'
-# })
-
-# if ENV["ACTIVE_RECORD_ENV"] == "test"
-#   ActiveRecord::Migration.verbose = false
-# end
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
